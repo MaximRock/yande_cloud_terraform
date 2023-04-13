bcf3ed03496ee48143a9a339df17929354d6b70a
MaximRock <95434302+MaximRock@users.noreply.github.com>
2023-04-13 17:34:45 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,17 +10,17 @@ terraform {
     bucket     = "tf-state-bucket-testmaxim"
     region     = "ru-central1-a"
     key        = "issue1/lemp.tfstate"
-    access_key = "YCAJEXUIA2C7m0vBy-DGK_GM4"
-    secret_key = "YCNoc7kDI7qz8PPUF6NxldimwiQEZlsBgF7F5VmW"
+    access_key = ""
+    secret_key = ""
 
     skip_region_validation      = true
     skip_credentials_validation = true
   }
 }
 provider "yandex" {
-  token     = "y0_AgAAAABkpk0NAATuwQAAAADSwkfOmQ835bV7TKqjVbXe1ht1599AzVs"
-  cloud_id  = "b1gfqsibs2oth9v8tqp5"
-  folder_id = "b1gt036erts2va4jvqeq"
+  token     = ""
+  cloud_id  = ""
+  folder_id = ""
   zone      = "ru-central1-a"
 }
 
@@ -95,93 +95,3 @@ resource "yandex_lb_network_load_balancer" "my-load-balancer-testmaxim" {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-#resource "yandex_lb_network_load_balancer" "internal-lb-test" {
-#  name = "my-network-load-balancer-testmaxim"
-#
-#  attached_target_group {
-#    target_group_id = yandex_lb_target_group.testmaxim.id
-#    healthcheck {
-#      name = "http"
-#    }
-#  }
-#}
-
-
-
-
-
-
-#terraform {
-#  required_providers {
-#    yandex = {
-#      source = "yandex-cloud/yandex"
-#      version = "0.60.0"
-#    }
-#  }
-#}
-#provider "yandex" {
-#  token     = "y0_AgAAAABkpk0NAATuwQAAAADSwkfOmQ835bV7TKqjVbXe1ht1599AzVs"
-#  cloud_id  = "b1gfqsibs2oth9v8tqp5"
-#  folder_id = "b1gt036erts2va4jvqeq"
-#  zone      = "ru-central1-a"
-#}
-#
-#data "yandex_compute_image" "my_image" {
-#  family = "lemp"
-#}
-#
-#resource "yandex_compute_instance" "vm-1" {
-#  name = "terraform1"
-#
-#  resources {
-#    cores  = 2
-#    memory = 2
-#  }
-#
-#  boot_disk {
-#    initialize_params {
-#      image_id = data.yandex_compute_image.my_image.id
-#    }
-#  }
-#
-#  network_interface {
-#    subnet_id = yandex_vpc_subnet.subnet-1.id
-#    nat       = true
-#  }
-#
-#  metadata = {
-#    ssh-keys = "ubuntu:${file("C:\\Users\\User\\.ssh/id_ed25519.pub")}"
-#  }
-#}
-#
-#resource "yandex_vpc_network" "network-yandex" {
-#  name = "network1"
-#}
-#
-#resource "yandex_vpc_subnet" "subnet-1" {
-#  name           = "subnet1"
-#  zone           = "ru-central1-a"
-#  network_id     = yandex_vpc_network.network-yandex.id
-#  v4_cidr_blocks = ["192.168.10.0/24"]
-#}
-#
-#output "internal_ip_address_vm_1" {
-#  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
-#}
-#
-#output "external_ip_address_vm_1" {
-#  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
-#}
